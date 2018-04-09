d06d95c850681d6141feea365b53bb365929c2e5
Krish <krishkrash2305@gmail.com>
2018-04-09 21:24:45 +0800

    Add a round function for simplicity

--- src/PreRelease.java
+++ src/PreRelease.java
@@ -27,7 +27,7 @@ public class PreRelease {
 
         for (int count = 0; count < totalCow; count++) {
             int identityOrder = count + 1;
-            System.out.println("Input the cow identity for cow #" + identityOrder);
+            System.out.println("Input the cow identity for cow #" + identityOrder + ".");
             cowID = scanner.nextInt();
 
             for (int counter = 0; counter < totalCow; counter++) {
@@ -50,7 +50,7 @@ public class PreRelease {
         for (int day = 0; day < 7; day++) {
             for (int session = 0; session < 2; session++) {
                 for (int i = 0; i < totalCow; i++) {
-                    System.out.println("What is the identity of the cow to be milked on day #" + (day + 1) + " session #" + (session + 1));
+                    System.out.println("Enter the identity of the cow to be milked on day #" + (day + 1) + " session #" + (session + 1) + ".");
                     cowID = scanner.nextInt();
 
                     for (int count = 0; count < totalCow; count++) {
@@ -63,7 +63,7 @@ public class PreRelease {
                         cowID = scanner.nextInt();
                     }
 
-                    System.out.println("What is the yield of cow #" + cowID);
+                    System.out.println("Enter the yield of cow #" + cowID + ".");
                     double yieldL = scanner.nextDouble();
 
                     for (int counter = 0; counter < totalCow; counter++) {
@@ -73,11 +73,13 @@ public class PreRelease {
                             totalYield += yieldL;
                             dailyYield[counter] += yieldL;
 
+                            /*
                             if (yield[counter] % 1 < 0.5) {
                                 yield[counter] = yield[counter] - (yield[counter] % 1);
                             } else {
                                 yield[counter] = yield[counter] + (1 - (yield[counter] % 1));
                             }
+                            */
 
                             if (session == 1) {
                                 if (dailyYield[counter] < 12) {
@@ -91,27 +93,34 @@ public class PreRelease {
             }
         }
 
-        System.out.println("The final yield of all the cows is " + totalYield + " litres of milk");
+        System.out.println("The final yield of all the cows is " + totalYield + " litres of milk.");
 
         for (int counter = 0; counter < totalCow; counter++) {
             double average = (yield[counter] / 7);
+            /*
             if (average % 1 < 0.5) {
                 average = average - (average % 1);
             } else {
                 average = average + (1 - (average % 1));
             }
-            System.out.println("The average yield for each cow in a day for identity " + identity[counter] + ": " + average + " litres of milk");
+            */
+            System.out.println("The average yield for each cow in a day for identity " + identity[counter] + ": " + round(average) + " litres of milk.");
             if (yield[counter] > highestYield) {
                 highestYield = yield[counter];
             }
             if (yield[counter] == highestYield) {
-                System.out.println("The cow identity: " + identity[counter] + " has the highest production of " + highestYield + ".");
+                System.out.println("The cow identity: " + identity[counter] + " has the highest production of " + round(highestYield) + ".");
             }
             if (low[counter] > 3) {
                 System.out.println("identity:" + identity[counter] + " has produced less than 12 litres of milk for at least 4 days.");
             }
         }
     }
+
+    private static int round(double num) {
+        int rounded = (int) num;
+        if (num % 1 >= 0.5) rounded++;
+        return rounded;
+    }
+
 }
-
-
